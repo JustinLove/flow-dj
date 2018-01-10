29defd102b713a3f08eaea607aeda4d0c60f9f17
Justin Love <git@JustinLove.name>
2018-01-10 12:59:09 -0600

    working on debug output in pick screen for development

--- BGAnimations/ScreenFlowDJPick overlay.lua
+++ BGAnimations/ScreenFlowDJPick overlay.lua
@@ -3,18 +3,60 @@ local stepstype = GAMESTATE:GetCurrentStyle(pn):GetStepsType()
 
 setenv("FlowDJ", true)
 
+local function SongDebug(song)
+	return song:GetDisplayMainTitle() ..  " " ..
+		song:GetDisplayBpms()[1] .. "-" ..
+		song:GetDisplayBpms()[2]
+end
+
+local function SongsDebug(songs)
+	local debug = {}
+	for i,song in ipairs(songs) do
+		debug[i] = SongDebug(song)
+	end
+	return table.concat(debug, "\n")
+end
+
+local function Truncate(table, to)
+	local truncated = {}
+	local length = math.min(to, #table)
+	for i=1,length do
+		truncated[i] = table[i]
+	end
+	return truncated
+end
+
+local function RemoveUnwantedGroups(songs)
+	local filtered_songs = {}
+	for i,song in ipairs(songs) do
+		if song:GetGroupName() ~= "Muted" and song:GetGroupName() ~= "Impossible" then
+			table.insert(filtered_songs, song)
+		end
+	end
+	return filtered_songs
+end
+
+local debug_text = false
+
 local function SetupNextGame()
-	local all_songs = SONGMAN:GetAllSongs()
+	local all_songs = RemoveUnwantedGroups(SONGMAN:GetAllSongs())
 	local song = all_songs[1]
+	debug_text:settext(SongsDebug(Truncate(all_songs, 16)))
 	local song_steps = song:GetStepsByStepsType(stepstype)
 	local steps = song_steps[1]
 	GAMESTATE:SetCurrentSong(song)
 	GAMESTATE:SetCurrentSteps(pn, steps)
 end
 
+local runonce = false
 local function update()
 	SetupNextGame()
-	trans_new_screen("ScreenGameplay")
+	--trans_new_screen("ScreenGameplay")
+	if not runonce then
+		runonce = true
+		--SetupNextGame()
+		--debug_text:settext(tries)
+	end
 end
 
 return Def.ActorFrame{
@@ -24,6 +66,9 @@ return Def.ActorFrame{
 		end,
 		Def.BitmapText{
 			Name = "Notice", Font = "Common Normal", InitCommand = function(self)
+				debug_text = self
+				self:maxwidth(SCREEN_HEIGHT)
+				self:zoom(0.5)
 				self:xy(_screen.cx, _screen.cy)
 			end
 		}
--- Scripts/00 Support.lua
+++ Scripts/00 Support.lua
@@ -2,3 +2,25 @@ function trans_new_screen(name)
 	SCREENMAN:GetTopScreen():SetNextScreenName(name)
 	SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
 end
+
+function rec_print_table_to_str(t, indent, depth_remaining)
+	if not indent then indent= "" end
+	if type(t) ~= "table" then
+		return indent .. "rec_print_table passed a " .. type(t)
+	end
+	depth_remaining= depth_remaining or -1
+	if depth_remaining == 0 then return "" end
+	local lines= {}
+	for k, v in pairs(t) do
+		if type(v) == "table" then
+			table.insert(lines, indent .. k .. ": table")
+			table.insert(lines, rec_print_table_to_str(
+										 v, indent .. "  ", depth_remaining - 1))
+		else
+			table.insert(lines, indent .. "(" .. type(k) .. ")" .. k .. ": " ..
+							"(" .. type(v) .. ")" .. tostring(v) .. "")
+		end
+	end
+	table.insert(lines, indent .. "end")
+	return table.concat(lines, "\n")
+end
