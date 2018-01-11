ea72be13013ce1529dfc40584ff88386922e1515
Justin Love <git@JustinLove.name>
2018-01-11 10:49:04 -0600

    graph of rating vs notes per second

--- BGAnimations/ScreenFlowDJPick overlay.lua
+++ BGAnimations/ScreenFlowDJPick overlay.lua
@@ -26,6 +26,14 @@ local function Truncate(table, to)
 	return truncated
 end
 
+function calc_nps(pn, song_len, steps)
+	local radar= steps:GetRadarValues(pn)
+	local notes= radar:GetValue("RadarCategory_TapsAndHolds") +
+		radar:GetValue("RadarCategory_Jumps") +
+		radar:GetValue("RadarCategory_Hands")
+	return notes / song_len
+end
+
 local function RemoveUnwantedGroups(songs)
 	local filtered_songs = {}
 	for i,song in ipairs(songs) do
@@ -36,6 +44,7 @@ local function RemoveUnwantedGroups(songs)
 	return filtered_songs
 end
 
+local graph = false
 local debug_text = false
 
 local function SetupNextGame()
@@ -48,15 +57,39 @@ local function SetupNextGame()
 	GAMESTATE:SetCurrentSteps(pn, steps)
 end
 
-local runonce = false
-local function update()
-	SetupNextGame()
-	--trans_new_screen("ScreenGameplay")
-	if not runonce then
-		runonce = true
-		--SetupNextGame()
-		--debug_text:settext(tries)
+local function AddPoint(x, y)
+	graph:AddChildFromPath(THEME:GetPathG("", "point.lua"))
+	local points = graph:GetChild("point")
+	if points and #points > 0 then
+		points[#points]:xy(20 + x * 90, 20 + y * 25)
 	end
+end
+
+local function GraphSteps()
+	graph:RemoveAllChildren()
+	local max_nps = 0
+	local all_songs = RemoveUnwantedGroups(SONGMAN:GetAllSongs())
+	for i, song in ipairs(all_songs) do
+		local song_steps = song:GetStepsByStepsType(stepstype)
+		local song_length = song:GetLastSecond() - song:GetFirstSecond()
+		for i, steps in ipairs(song_steps) do
+			local rating = steps:GetMeter()
+			local nps = calc_nps(pn, song_length, steps)
+			AddPoint(nps, rating)
+			max_nps = math.max(max_nps, nps)
+		end
+	end
+	nps_text:settext(max_nps)
+end
+
+local frame = 0
+local function update()
+	--SetupNextGame()
+	--trans_new_screen("ScreenGameplay")
+	if frame == 1 then
+		GraphSteps()
+	end
+	frame = frame + 1
 end
 
 return Def.ActorFrame{
@@ -64,13 +97,25 @@ return Def.ActorFrame{
 		Name = "Picker", OnCommand = function(self)
 			self:SetUpdateFunction(update)
 		end,
-		Def.BitmapText{
-			Name = "Notice", Font = "Common Normal", InitCommand = function(self)
-				debug_text = self
-				self:maxwidth(SCREEN_HEIGHT)
-				self:zoom(0.5)
-				self:xy(_screen.cx, _screen.cy)
-			end
-		}
+	},
+	Def.BitmapText{
+		Name = "Notice", Font = "Common Normal", InitCommand = function(self)
+			debug_text = self
+			self:maxwidth(SCREEN_HEIGHT)
+			self:zoom(0.5)
+			self:xy(_screen.cx, _screen.cy)
+		end
+	},
+	Def.BitmapText{
+		Name = "NPS", Font = "Common Normal", InitCommand = function(self)
+			nps_text = self
+			self:maxwidth(SCREEN_HEIGHT)
+			self:xy(SCREEN_WIDTH - 50, SCREEN_HEIGHT - 50)
+		end
+	},
+	Def.ActorFrame{
+		Name= "graph", InitCommand= function(self)
+			graph = self
+		end
 	}
 }
---
+++ Graphics/point.lua
@@ -0,0 +1,6 @@
+return Def.ActorFrame{
+	Def.Quad{
+		Name= "dot", InitCommand = cmd(setsize, 0.2, 10)
+	},
+	Name= "point", InitCommand= cmd(visible, true),
+}
